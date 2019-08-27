68ee761b5c040559c8337b6ad4f5b5ec3d9617f2
Ethan Weber <Ethan.Weber@molex.com>
2019-08-27 14:50:07 -0500

    attempted and failed 66

---
+++ ProjectEuler/Problems/1-100/61-70/Problem66.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem66
+    {
+        public void Method2()
+        {
+            Console.WriteLine("Considering Diophantine equations of the form 'x^2 - D*y^2 = 1', find the value " +
+                              "of D <= 1000 in minimal solutions of x for which the largest value of x is obtained");
+
+            // my solution was wrong, do again some time
+        }
+    }
+}
