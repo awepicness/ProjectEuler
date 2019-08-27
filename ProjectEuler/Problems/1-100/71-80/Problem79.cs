90a7a5094457e7d771ac732d881a108c8f58e95c
Ethan Weber <Ethan.Weber@molex.com>
2019-08-27 13:02:36 -0500

    finished problem 79

---
+++ ProjectEuler/Problems/1-100/71-80/Problem79.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProjectEuler.Problems
+{
+    class Problem79
+    {
+        public void Method()
+        {
+            Console.WriteLine("Given a file with 50 successful login attempts of a non-contiguous subsequence of 3 characters " +
+                              "of the password where the characters are asked for in order, analyse the file to determine the password");
+
+            // const string filename = "79keylog.txt";
+
+            // don't know how to solve it through code, but easily got the answer on paper
+            Console.WriteLine("Password: 73162890");
+        }
+    }
+}
