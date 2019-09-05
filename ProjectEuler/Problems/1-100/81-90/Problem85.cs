1d5384fe4d0446c255cb8d881974d78ad2a38d21
Ethan Weber <Ethan.Weber@molex.com>
2019-09-05 17:01:18 -0500

    solve 85
    updated 73

---
+++ ProjectEuler/Problems/1-100/81-90/Problem85.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ProjectEuler.Problems
+{
+    class Problem85
+    {
+        public void Method()
+        {
+            Console.WriteLine("Although there exists no rectangular grid that can be sectioned into 2 million rectangles, " +
+                              "find the area of the grid with the nearest solution");
+
+            // formula: n*m*(n+1)*(m+1)/4 = number of sub-rectangles in an n x m rectangle
+
+            // max value of n^2 / 4 = 2000000 solved for n is 2828.4
+            const int nMax = 2001;
+            const int searchValue = 2000000;
+
+            int min = int.MaxValue;
+            int nMin = 0, mMin = 0, nBymMin = 0;
+            for (int n = 1; n < nMax; n++)
+            {
+                for (int m = 1; m <= n; m++)
+                {
+                    int cur = n * m * (n + 1) * (m + 1) / 4;
+                    if (cur > searchValue)
+                        break;
+
+                    int abs = Math.Abs(searchValue - cur);
+                    if (abs >= min)
+                        continue;
+
+                    min = abs;
+                    nMin = n;
+                    mMin = m;
+                    nBymMin = cur;
+                }
+            }
+            Console.WriteLine($"an {nMin}*{mMin} rectangle has {nBymMin} subs and an area of {nMin * mMin}");
+        }
+    }
+}
