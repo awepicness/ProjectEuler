a7c31311b87326343a1ddee750875e613915af2d
Ethan Weber <Ethan.Weber@molex.com>
2019-09-17 11:49:16 -0500

    finish 197

---
+++ ProjectEuler/Problems/101-200/191-200/Problem197.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    class Problem197
+    {
+        private const double twoPow = 30.403243784;
+        private const double tenPow = 1E-09;
+        public void Method()
+        {
+            Console.WriteLine("Given the recursive function f(x) (coded below), find u[n] + u[n+1] for n = 10^12");
+
+            // I don't understand it completely, but originally made a dictionary and noticed the 
+            // values when i was a factor of 1001 alternated between 2 values, as if the function made a loop.
+            // so I just added those values and got the right answer
+
+            double u = -1.0; // u[0] = -1
+            double sum = 0.0;
+            for(long i = 1; i <= 2002; i++)
+            {
+                u = func(u);
+                if(i % 1001 == 0)
+                    sum += u;
+            }
+
+            Console.WriteLine($"result: {sum}");
+        }
+
+        private double func(double x) => Math.Floor(Math.Pow(2, twoPow - x * x)) * tenPow;
+        
+    }
+}
