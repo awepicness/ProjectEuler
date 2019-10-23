86d94fa5804141912716a88ba71e7b9ba5b99ff1
awepicness <ethanrweber@gmail.com>
2019-10-23 15:33:16 -0500

    finish 135

--- ProjectEuler/Problems/101-200/131-140/Problem135.cs
+++ ProjectEuler/Problems/101-200/131-140/Problem135.cs
@@ -1,50 +1,41 @@
 ﻿using System;
+using System.Linq;
 
 namespace ProjectEuler.Problems
 {
     class Problem135
     {
-        public void Method2()
+        public void Method()
         {
             Console.WriteLine("for values of a number n from 1 to 1 million, determine the number of solutions " +
                               "for n in which n can be expressed as z^2-y^2-x^2 in exactly 10 unique ways, " +
                               "where x,y,z are terms in a series of arithmetic progression");
 
+            // as an arithmetic progression, z = x + 2*d and y = x + d
+            // so the eqn can be rewritten as (x+2d)^2 - (x+d)^2 - x^2 = n
+            // this can be further simplified as -(x-3d)*(d+x) = n
+            // or more simply (3d-x)*(d+x) = n
+            // we can rewrite this by substituting in variables for the values in parentheses
+            // such that u = (3d-x) and v = (d+x)
+            // putting this back in terms of x and d, we see that
+            // d = (u + v) / 4 and x = (3*v - u)/4
+
             // it is given that the least value with 10 solutions is 1155, so we can start there
-            const int nLimit = 2000;
+            const int n = 1000000;
             const int uniqueWays = 10;
 
-            int solutions = 0;
-            for (int n = 1155; n < nLimit; n++)
+            int[] solutions = new int[n+1];
+            for (int u = 1; u <= n; u++)
             {
-                //if(n%10==0)
-                //    Console.WriteLine(n);
-                int count = 0;
-                int distance = 0;
-                while (distance < n/2)
+                for (int v = 1; u*v <= n; v++)
                 {
-                    distance++;
-                    for (int x = 1; x < n; x++)
-                    {
-                        // simplification of (x+2d)^2 - (x+d)^2 - x^2
-                        int v = -(x - 3 * distance) * (distance + x);
-                        //if(x%10==0 && distance%10==0)
-                        //    Console.WriteLine($"x:{x}, d:{distance}, v:{v}");
-                        if (v == n)
-                        {
-                            count++;
-                            //Console.WriteLine("count++!");
-                            //Console.WriteLine($"x:{x}, d:{distance}, v:{v}");
-                        }
-                    }
+                    if ((u + v) % 4 == 0 && 3 * v > u && (3 * v - u) % 4 == 0)
+                        solutions[u * v]++;
                 }
-
-                if (count == uniqueWays)
-                    solutions++;
             }
+            int result = solutions.Count(x => x == uniqueWays);
             
-
-            Console.WriteLine($"There are {solutions} solutions for n < {nLimit}");
+            Console.WriteLine($"There are {result} solutions < {n} where n can be expressed in {uniqueWays} unique ways");
         }
     }
 }
